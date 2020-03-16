d11e2a06ec6636717bfd33e214cfa081d39f259a
omarMohamedAbdo <omarknighto@gmail.com>
2020-03-16 19:11:18 +0200

    updating Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Agency - BookSoulsLibrary</title>
+        
 
         <!-- Bootstrap core CSS -->
          <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
@@ -52,6 +53,9 @@
                     <li class="nav-item">
                         <a class="nav-link js-scroll-trigger" href="#services">Services</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#team">Our Team</a>
+                    </li>
                     </ul>
                 </div>
                 </div>
@@ -74,10 +78,102 @@
       <div class="row">
         <div class="col-lg-12 text-center">
           <h2 class="section-heading text-uppercase">Services</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 class="section-subheading text-muted">Always at your help</h3>
         </div>
+        <div class="row text-center">
+        <div class="col-md-4">
+          <h4 class="service-heading">Digital Entertainment</h4>
+          <p class="text-muted">You can also check out CDs, DVDs, audiobooks, and video games from most public libraries.</p>
+        </div>
+        <div class="col-md-4">
+          <h4 class="service-heading">Leases</h4>
+          <p class="text-muted">Here you can borrow any books any time, but you have maximum of 1 month per book.</p>
+        </div>
+        <div class="col-md-4">
+          <h4 class="service-heading">Academic & Research Support</h4>
+          <p class="text-muted">You can also turn to the public library if you or someone else in your family needs help on a school project or research paper.</p>
+        </div>
+      </div>
       </div>
   </section>
 
+  <!-- Team -->
+  <section class="bg-light page-section" id="team">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
+          <h3 class="section-subheading text-muted">Awesome Developers</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="{{asset('imgs/omar.jpeg')}}" alt="">
+            <h4>Omar Abdo</h4>
+            <p class="text-muted">Super Developer</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="{{asset('imgs/shefo.jpeg')}}" alt="">
+            <h4>Mohamed Shafeay</h4>
+            <p class="text-muted">Software Developer</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="{{asset('imgs/pp.jpeg')}}" alt="">
+            <h4>Mohamed Nagieb</h4>
+            <p class="text-muted">Software Developer</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="{{asset('imgs/header-bg.jpg')}}" alt="">
+            <h4>Reham Hussien</h4>
+            <p class="text-muted">Software Developer</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 mx-auto text-center">
+          <p class="large text-muted">This Team Work 24 hours daily for your pleasure</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4">
+          <span class="copyright">Copyright &copy; Book Souls 2020</span>
+        </div>
+        <div class="col-md-4">
+          
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline quicklinks">
+            <li class="list-inline-item">
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/jquery.easing.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+
+  <script type="text/javascript" src="{{asset('js/agency.min.js')}}"></script>
     </body>
 </html>
