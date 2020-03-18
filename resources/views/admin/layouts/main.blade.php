18ebecd21fa8e9b9e91ad25121a745073cc9b6a3
omarMohamedAbdo <omarknighto@gmail.com>
2020-03-19 00:49:28 +0200

    Updating About, Dashboard & login styles

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -30,7 +30,7 @@
             <div class="app-header__logo">
                 <!-- <div class="logo-src"></div> -->
                 <a class="navbar-brand" href="/">
-                    <img src="{{asset('imgs/LogoMakr_0kpqd2.png')}}" alt="Logo" style="width:180px;height:75px;margin-top:5px;">
+                    <img src="{{asset('imgs/LogoMakr_8FEAlV.png')}}" alt="Logo" style="width:180px;height:75px;margin-top:5px;">
                 </a>
                 <div class="header__pane ml-auto">
                     <div>
