750f06bf30010a229cb2b9f4b1f81b5259c9c69e
Mohammed Naguib <m.naguib2611@gmail.com>
2020-08-18 00:27:16 +0200

    heroku-deployment

--- database/factories/LeaseFactory.php
+++ database/factories/LeaseFactory.php
@@ -5,13 +5,16 @@
 use App\Lease;
 use App\User;
 use App\Book;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Lease::class, function (Faker $faker) {
     Lease::flushEventListeners();
+    $duration=mt_rand(1, 10);
     return [
         'user_id'  =>User::inRandomOrder() -> first() -> id,
         'book_id'  =>Book::inRandomOrder() -> first() -> id,
-        'duration' =>mt_rand(1, 10),
+        'duration' =>$duration,
+        'end_date' =>Carbon::now()->addDays($duration),
     ];
 });
